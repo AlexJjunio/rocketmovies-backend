0d30b8f5ae2e34d105fad7f5558de95bbde56cdb
iJjunio <alex_jjunio@outlook.com>
2023-04-18 00:32:36 -0300

    Testing app and installing nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.get("/users", (request, response) => {
+  const {page, limit} = request.query;
+
+  response.send(`
+    Page: ${page}. 
+    Limit: ${limit} `)
+});
+
+app.get("/", (req, res) => {
+  res.send('Testando')
+})
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
