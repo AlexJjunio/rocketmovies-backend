574911f0c9fe5c7846c896ecda070427acfbf17b
iJjunio <alex_jjunio@outlook.com>
2023-04-20 10:53:41 -0300

    Organizing project structure

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/users", (request, response) => {
-  const {page, limit} = request.query;
-
-  response.send(`
-    Page: ${page}. 
-    Limit: ${limit} `)
-});
-
-app.get("/", (req, res) => {
-  res.send('Testando')
-})
-
 const PORT = 3333;
-
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+
+const routes = require("./routes")
+
+app.use(express.json())
+
+app.use(routes);
