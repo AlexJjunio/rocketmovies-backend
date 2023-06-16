1ff5e12bcca31c43aa3f25b4ee23c592ffba99c8
iJjunio <alex_jjunio@outlook.com>
2023-06-15 23:45:39 -0300

    exception handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,17 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
   create( req,res ) {
     const { name, email, password } = req.body;
+    
+    if(!name) {
+      throw new AppError("Você não informou o nome.")
+    }
+
+    if(!password) {
+      throw new AppError("Senha é obrigatório")
+    }
+
     res.status(201).json({name, email, password})
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
+require("express-async-errors");
+const AppError = require("./utils/AppError");
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.json())
 
-const routes = require("./routes")
+const routes = require("./routes");
+
+app.use(routes);
+
+app.use((error, req, res, next) => {
+  
+  if(error instanceof AppError) {
+    res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+  
+  console.log(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "internal server error"
+  });
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
-
-app.use(routes);
